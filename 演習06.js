5b1dee874c0dedb019749d2d627330389aba463e
shimizuyoshiki <2185039@stu.hus.ac.jp>
2020-07-02 13:47:04 +0900

    演習06.html

---
+++ 演習06.js
@@ -0,0 +1,11 @@
+for(let i =1; i  <=30; i++){
+if(i%15 ===0){
+    console.log("fizz Buzz")
+    }else if(i%3 ===0 ){
+        console.log("fizz")
+    } else if(i%5 ===0){
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
